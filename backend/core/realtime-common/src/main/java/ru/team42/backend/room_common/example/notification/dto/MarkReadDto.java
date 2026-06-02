7be7f322b20eaefd718f3a1a2d4bcf9e85320d4a
Idzey <kir.pan.787@gmail.com>
2026-06-03 00:02:17 +0300

    Merge branch 'master' of https://github.com/42team-ru/digital-penetration

--- backend/core/realtime-common/src/main/java/ru/team42/backend/room_common/example/notification/dto/MarkReadDto.java
+++
@@ -1,8 +0,0 @@
-package ru.team42.backend.room_common.example.notification.dto;
-
-import lombok.Data;
-
-@Data
-public class MarkReadDto {
-    private String notificationId;
-}
--- backend/core/realtime-common/src/main/java/ru/team42/backend/room_common/example/poll/dto/VoteDto.java
+++
@@ -1,8 +0,0 @@
-package ru.team42.backend.room_common.example.poll.dto;
-
-import lombok.Data;
-
-@Data
-public class VoteDto {
-    private String optionId;
-}
