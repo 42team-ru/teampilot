e4e7158d8529efc6a6a06308d4db14102bf13099
Vladimir Melnik <hexaend@mail.ru>
2026-06-03 00:29:28 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.gitignore

---
+++ backend/monolith/src/main/java/ru/team42/monolith/consumer/ChatMessageConsumer.java
@@ -0,0 +1,27 @@
+package ru.team42.monolith.consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+import ru.team42.backend.kafka_common.event.ChatMessageEvent;
+import ru.team42.backend.kafka_common.event.KafkaTopics;
+import ru.team42.monolith.service.ChatMessageService;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatMessageConsumer {
+
+    private final ChatMessageService chatMessageService;
+
+    @KafkaListener(topics = KafkaTopics.MESSAGES_RAW)
+    public void consume(List<ChatMessageEvent> messages, Acknowledgment ack) {
+        log.info("Received batch of {} chat messages", messages.size());
+        chatMessageService.saveAll(messages);
+        ack.acknowledge();
+    }
+}
