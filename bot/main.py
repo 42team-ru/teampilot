c4fdc4897c1a073896e8c765e02c39fdef70a803
Idzey <kir.pan.787@gmail.com>
2026-06-07 22:39:21 +0300

    42

--- bot/main.py
+++ bot/main.py
@@ -22,7 +22,6 @@ from handlers.manager import router as manager_router
 from handlers.member import router as member_router
 from handlers.profile import router as profile_router
 from handlers.registration import router as registration_router
-from handlers.reminders import router as reminders_router
 from handlers.setup import router as setup_router
 from handlers.sync import router as sync_router
 from handlers.sync import sync_report_router
@@ -156,7 +155,6 @@ async def main() -> None:
     dp.include_router(member_router)
     dp.include_router(auth_router)
     dp.include_router(sync_router)
-    dp.include_router(reminders_router)
     dp.include_router(tasks_router)
     dp.include_router(tasks_commands_router)
     dp.include_router(upload_router)
